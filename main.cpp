75511145259119223bbe555ce6ba7af7cee6803f
Jakob Weiss <jakob.jw.weiss@gmail.com>
2020-10-27 00:04:15 +0100

    First GLFW window init

--- main.cpp
+++ main.cpp
@@ -1,5 +1,61 @@
-#include <spdlog/spdlog.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-int main(int argc, char** argv){
-    spdlog::info("Spdlog is running!");
+#include <spdlog/spdlog.h>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+public:
+	static constexpr uint32_t WIDTH = 800;
+	static constexpr uint32_t HEIGHT = 600;
+
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	void initWindow() {
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		m_window = glfwCreateWindow(WIDTH, HEIGHT, "VK Tutorial", nullptr, nullptr);
+	}
+
+	void initVulkan() {
+
+	}
+
+	void mainLoop() {
+		while (!glfwWindowShouldClose(m_window)) {
+			glfwPollEvents();
+		}
+	}
+
+	void cleanup() {
+		glfwDestroyWindow(m_window);
+
+		glfwTerminate();
+	}
+
+	GLFWwindow* m_window;
+};
+
+int main() {
+	HelloTriangleApplication app;
+
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		spdlog::error("Unhandled exception: {}", e.what());
+
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }

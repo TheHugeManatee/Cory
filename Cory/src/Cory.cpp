cfd84bea2e8ede9d9b09ccd203be2faf3f701bdd
Jakob Weiss <jakob.jw.weiss@gmail.com>
2022-09-10 14:15:33 +0200

    proof of concept integration into library

--- Cory/src/Cory.cpp
+++ Cory/src/Cory.cpp
@@ -1,7 +1,15 @@
 #include <Cory/Cory.hpp>
 
+#include <Magnum/Vk/Version.h>
+
+#include <fmt/core.h>
+
 namespace Cory {
-    int test_function () {
-        return 42;
-    }
+int test_function() { return 42; }
+std::string queryVulkanInstanceVersion()
+{
+    auto version = Magnum::Vk::enumerateInstanceVersion();
+    return fmt::format(
+        "{}.{}.{}", versionMajor(version), versionMinor(version), versionPatch(version));
 }
+} // namespace Cory

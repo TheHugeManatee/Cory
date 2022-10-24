713122565450c8c5be647b8ea13eb90a979baddd
Jakob Weiss <jakob.jw.weiss@gmail.com>
2022-10-24 23:54:43 +0200

    add more passes to example

--- Cory/test/FrameGraph_Test.cpp
+++ Cory/test/FrameGraph_Test.cpp
@@ -8,8 +8,12 @@
 #include <cppcoro/sync_wait.hpp>
 #include <cppcoro/when_all.hpp>
 
+#include <gsl/gsl>
+
 using namespace Cory;
 namespace FG = Cory::Framegraph;
+
+namespace passes {
 
 struct DepthPassOutputs {
     FG::TextureHandle depthTexture;
@@ -25,8 +29,64 @@ FG::RenderPassDeclaration<DepthPassOutputs> depthPass(FG::Framegraph &graph, glm
     co_yield outputs;
     FG::RenderInput render = co_await builder.finishDeclaration();
 
-    CO_APP_INFO("DepthPrepass render commands executing");
-    
+    CO_APP_INFO("[DepthPrepass] render commands executing");
+}
+
+struct DepthDebugOut {
+    FG::TextureHandle debugColor;
+};
+FG::RenderPassDeclaration<DepthDebugOut> depthDebug(FG::Framegraph &graph,
+                                                    FG::TextureHandle depthInput)
+{
+    FG::Builder builder = graph.declarePass("DepthDebug");
+
+    FG::TextureHandle depth = co_await builder.read(depthInput);
+    FG::MutableTextureHandle depthVis = co_await builder.create(
+        "depthDebugVis", depth.size, FG::PixelFormat::RGBA32, FG::Layout::ColorAttachment);
+
+    co_yield DepthDebugOut{depthVis};
+    FG::RenderInput render = co_await builder.finishDeclaration();
+
+    CO_APP_INFO("[DepthDebug] Pass render commands are executed");
+}
+
+struct NormalDebugOut {
+    FG::TextureHandle debugColor;
+};
+FG::RenderPassDeclaration<NormalDebugOut> normalDebug(FG::Framegraph &graph,
+                                                      FG::TextureHandle normalInput)
+{
+    FG::Builder builder = graph.declarePass("NormalDebug");
+
+    FG::TextureHandle normal = co_await builder.read(normalInput);
+    FG::MutableTextureHandle normalVis = co_await builder.create(
+        "normalDebugVis", normalInput.size, FG::PixelFormat::RGBA32, FG::Layout::ColorAttachment);
+
+    co_yield NormalDebugOut{normalVis};
+    FG::RenderInput render = co_await builder.finishDeclaration();
+
+    CO_APP_INFO("[NormalDebug] Pass render commands are executed");
+}
+
+struct DebugOut {
+    FG::TextureHandle debugColor;
+};
+FG::RenderPassDeclaration<DebugOut> debugGeneral(FG::Framegraph &graph,
+                                                 std::vector<FG::TextureHandle> debugTextures,
+                                                 gsl::index debugViewIndex)
+{
+    FG::Builder builder = graph.declarePass("GeneralDebug");
+
+    auto &textureToDebug = debugTextures[debugViewIndex];
+    FG::TextureHandle depth = co_await builder.read(textureToDebug);
+
+    FG::MutableTextureHandle depthVis = co_await builder.create(
+        "debugVis", textureToDebug.size, FG::PixelFormat::RGBA32, FG::Layout::ColorAttachment);
+
+    co_yield DebugOut{depthVis};
+    FG::RenderInput render = co_await builder.finishDeclaration();
+
+    CO_APP_INFO("[Debug] Pass render commands are executed");
 }
 
 struct MainOut {
@@ -53,20 +113,59 @@ FG::RenderPassDeclaration<MainOut> mainPass(FG::Framegraph &graph, FG::TextureHa
         FG::Texture normal;
     };
 
-    CO_APP_INFO("Rendering main pass");
+    CO_APP_INFO("[MainPass] Pass render commands are executed");
 }
+
+struct PostProcessOut {
+    FG::TextureHandle color;
+};
+FG::RenderPassDeclaration<PostProcessOut> postProcess(FG::Framegraph &graph,
+                                                      FG::TextureHandle currentColorInput,
+                                                      FG::TextureHandle previousColorInput)
+{
+    FG::Builder builder = graph.declarePass("Postprocess");
+
+    FG::TextureHandle curColor = co_await builder.read(currentColorInput);
+    FG::TextureHandle prevColor = co_await builder.read(previousColorInput);
+    FG::MutableTextureHandle colorWritable = co_await builder.create(
+        "postprocessTexture", curColor.size, FG::PixelFormat::RGBA32, FG::Layout::ColorAttachment);
+
+    co_yield PostProcessOut{colorWritable};
+    FG::RenderInput render = co_await builder.finishDeclaration();
+
+    CO_APP_INFO("[Postprocess] Pass render commands are executed");
+}
+} // namespace passes
 
 TEST_CASE("Framegraph API exploration", "[Cory/Framegraph/Framegraph]")
 {
     FG::Framegraph fg;
-    auto depthPassTask = depthPass(fg, {800, 600, 1});
-    auto mainPassTask = mainPass(fg, depthPassTask.output().depthTexture);
+    FG::TextureHandle prevFrameColor = fg.declareInput("previousFrameColor");
 
-    auto mainOut = mainPassTask.output();
-    CO_APP_INFO("Main pass outputs a color texture of {}x{}x{}",
-                mainOut.color.size.x,
-                mainOut.color.size.y,
-                mainOut.color.size.z);
+    auto depthPass = passes::depthPass(fg, {800, 600, 1});
+    auto mainPass = passes::mainPass(fg, depthPass.output().depthTexture);
+
+    auto depthDebugPass = passes::depthDebug(fg, depthPass.output().depthTexture);
+    auto normalDebugPass = passes::normalDebug(fg, mainPass.output().normal);
+    auto debugCombinePass = passes::debugGeneral(
+        fg, {depthDebugPass.output().debugColor, normalDebugPass.output().debugColor}, 0);
+
+    auto postProcess = passes::postProcess(fg, mainPass.output().color, prevFrameColor);
+
+    // provoke the coroutines to run so we have some stuff to cull in the graph
+    depthDebugPass.output();
+    normalDebugPass.output();
+    debugCombinePass.output();
+
+    auto postprocessOut = postProcess.output();
+
+    fg.declareOutput(postprocessOut.color);
+
+    CO_APP_INFO("Final output is a color texture of {}x{}x{}",
+                postprocessOut.color.size.x,
+                postprocessOut.color.size.y,
+                postprocessOut.color.size.z);
+
 
     fg.execute();
 }

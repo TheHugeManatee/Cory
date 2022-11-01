59cf60b98c0157e1ff8ae8436a1ccd89b4cd80c7
Jakob Weiss <jakob.jw.weiss@gmail.com>
2022-11-01 14:27:47 +0100

    more fiddling with that blasted SlotMap

--- Cory/include/Cory/Base/SlotMap.hpp
+++ Cory/include/Cory/Base/SlotMap.hpp
@@ -1,44 +1,82 @@
 #pragma once
 
+#include <Cory/Base/Log.hpp>
 #include <fmt/core.h>
 
 #include <array>
-#include <cassert>
 #include <cstdint>
+#include <memory>
 #include <stdexcept>
 #include <vector>
 
 namespace Cory {
 
 struct SlotMapHandle {
+    static constexpr uint32_t INVALID_INDEX{0xFFFF'FFFF};
+    static constexpr uint32_t FREE_VERSION{0x8000'0000};
+    static constexpr uint64_t FREE_BIT{0x8000'0000'0000'0000};
+
     uint64_t v;
+
+    /// default-constructed handle has invalid version and index
+    SlotMapHandle()
+        : v{uint64_t{FREE_VERSION} << 32 | INVALID_INDEX}
+    {
+    }
+    /// construct a handle with given index and version
+    SlotMapHandle(uint32_t index, uint32_t version = 0)
+        : v{uint64_t{version} << 32 | index}
+    {
+    }
+    [[nodiscard]] uint32_t index() const noexcept { return v & 0xFFFF'FFFF; }
+    [[nodiscard]] uint32_t version() const noexcept { return v >> 32; }
+    [[nodiscard]] bool alive() const noexcept
+    {
+        return index() != INVALID_INDEX && (v & FREE_BIT) == 0;
+    }
+
+    [[nodiscard]] static SlotMapHandle nextVersion(SlotMapHandle old)
+    {
+#ifdef _DEBUG
+        CO_CORE_ASSERT((old.index() & 0x7FFF'FFFF) != 0x7FFF'FFFF, "Version rollover detected!");
+#endif
+        return {old.index(), old.version() + 1};
+    }
+    [[nodiscard]] static SlotMapHandle clearFreeBit(SlotMapHandle handle)
+    {
+        return {handle.index(), handle.version() & 0x7FFF'FFFF};
+    }
+    [[nodiscard]] static SlotMapHandle setFreeBit(SlotMapHandle handle)
+    {
+        return {handle.index(), handle.version() & 0x7FFF'FFFF | FREE_VERSION};
+    }
     auto operator<=>(const SlotMapHandle &rhs) const = default;
 };
 
-// simple generic slot map for associative storage of objects in contiguous memory
-// roughly following
-// https://web.archive.org/web/20190113012453/https://seanmiddleditch.com/data-structures-for-game-developers-the-slot-map/
-// It likely has some UB in there somewhere when allocating and placement-initializing the
-// chunks but it *seems* to work?
-// TODO:
-//   - Add support for types that chose to intrusively store their identifiers (via concepts?)
-//   - probably more optimal to change memory layout to avoid interleaving data and index
-//   - add iterator support (needs some additional in-use map?)
-//   - memory compaction support - implicit or on-demand? how even?
-template <typename StoredType> class SlotMap {
+/**
+ * @brief simple generic slot map for associative storage of objects in contiguous memory
+ * roughly following
+ * https://web.archive.org/web/20190113012453/https://seanmiddleditch.com/data-structures-for-game-developers-the-slot-map/
+ * It probably has some UB in there somewhere when allocating and placement-initializing the
+ * chunks but it *seems* to work (on msvc...)
+ * TODO:
+ *   - Add support for types that chose to intrusively store their identifiers (via concepts?)
+ *   - probably more optimal to change memory layout to avoid interleaving data and index
+ *   - memory compaction support - implicit or on-demand? how even?
+ */
+template <typename StoredType_> class SlotMap {
   public:
+    using StoredType = StoredType_;
     static constexpr size_t CHUNK_SIZE{64};
-    static constexpr uint32_t FREE_VERSION{0xFFFFFFFF};
 
-    StoredType &operator[](SlotMapHandle id)
+    [[nodiscard]] StoredType &operator[](SlotMapHandle id)
     {
-        assert(version(id) != FREE_VERSION);
-        // throw if handle out of date
-        const auto idx = index(id);
-        auto &object = objectAt(idx);
-        if (object.id != id) { throw std::range_error("Handle is no longer valid!"); }
-
+        auto &object = validatedGet(id);
         return reinterpret_cast<StoredType &>(object.storage);
+    }
+    [[nodiscard]] const StoredType &operator[](SlotMapHandle id) const
+    {
+        return const_cast<SlotMap &>(*this)[id];
     }
 
     template <typename... InitArgs> SlotMapHandle emplace(InitArgs... args)
@@ -46,19 +84,20 @@ template <typename StoredType> class SlotMap {
         // check if we need to allocate more chunks
         if (freeList_.empty()) {
             // allocate a new chunk and add the leftovers to the free list
-            const auto num_chunks = objectChunkTable_.size();
-            Chunk &chunk = *objectChunkTable_.emplace_back(std::make_unique<Chunk>());
+            const auto num_chunks = chunkTable_.size();
+            Chunk &chunk = *chunkTable_.emplace_back(std::make_unique<Chunk>());
 
             freeList_.reserve(CHUNK_SIZE);
 
             for (uint64_t i = CHUNK_SIZE - 1; i > 0; --i) {
                 const auto idx = num_chunks * CHUNK_SIZE + i;
                 // set version to free marker
-                chunk[i].id = makeHandle(static_cast<uint32_t>(idx), FREE_VERSION);
+                chunk[i].id =
+                    SlotMapHandle{static_cast<uint32_t>(idx), SlotMapHandle::FREE_VERSION};
                 freeList_.push_back(static_cast<uint32_t>(idx));
             }
             const auto idx = num_chunks * CHUNK_SIZE;
-            chunk[0].id = makeHandle(static_cast<uint32_t>(idx), 0);
+            chunk[0].id = SlotMapHandle{static_cast<uint32_t>(idx), 0};
 
             new (&chunk[0].storage) StoredType{std::forward<InitArgs>(args)...};
             return chunk[0].id;
@@ -68,9 +107,9 @@ template <typename StoredType> class SlotMap {
         int free = freeList_.back();
         freeList_.pop_back();
 
-        auto &object = objectAt(free);
-        // reset version to zero
-        object.id = makeHandle(index(object.id), 0);
+        auto &object = *objectAt(free);
+        CO_CORE_ASSERT(!object.id.alive(), "We got a live object from the free list!");
+        object.id = SlotMapHandle::clearFreeBit(object.id);
         // construct the object in the storage
         new (&object.storage) StoredType{std::forward<InitArgs>(args)...};
         return object.id;
@@ -86,23 +125,20 @@ template <typename StoredType> class SlotMap {
     // release the object, invalidating previous handles and reclaiming the memory for future use
     void release(SlotMapHandle id)
     {
-        const auto idx = index(id);
-        auto &object = objectAt(idx);
-        object.id = makeHandle(index(id), FREE_VERSION); // reset version to free marker
+        auto &object = validatedGet(id);
+        // increase version and set free bit
+        object.id = SlotMapHandle::setFreeBit(SlotMapHandle::nextVersion(id));
 
         reinterpret_cast<StoredType *>(object.storage)->~StoredType(); // destruct the object
-        freeList_.push_back(idx);
+        freeList_.push_back(id.index());
     }
 
     // update by assigning a new value, will invalidate old handles to the entry
     template <typename ArgumentType> SlotMapHandle update(SlotMapHandle id, ArgumentType &&value)
     {
-        assert(version(id) != FREE_VERSION);
-        const auto idx = index(id);
-        auto &object = objectAt(idx);
-        if (object.id != id) { throw std::range_error("Handle is no longer valid!"); }
+        auto &object = validatedGet(id);
         reinterpret_cast<StoredType &>(object.storage) = value; // update stored value
-        object.id = makeHandle(index(id), version(id) + 1);     // increase version
+        object.id = SlotMapHandle::nextVersion(id);
         return object.id;
     }
 
@@ -110,29 +146,57 @@ template <typename StoredType> class SlotMap {
     // use to reflect a semantic change in the value
     SlotMapHandle update(SlotMapHandle id)
     {
-        assert(version(id) != FREE_VERSION);
-        const auto idx = index(id);
-        auto &object = objectAt(idx);
-        if (object.id != id) { throw std::range_error("Handle is no longer valid!"); }
-        object.id = makeHandle(index(id), version(id) + 1); // increase version
+        auto &object = validatedGet(id);
+        object.id = SlotMapHandle::nextVersion(id); // increase version
         return object.id;
     }
 
-    size_t size() { return objectChunkTable_.size() * CHUNK_SIZE - freeList_.size(); }
+    size_t size() const noexcept { return chunkTable_.size() * CHUNK_SIZE - freeList_.size(); }
+    size_t capacity() const noexcept { return chunkTable_.size() * CHUNK_SIZE; }
+
+    bool isValid(SlotMapHandle id) const
+    {
+        if (!id.alive()) return false;
+
+        auto *object = objectAt(id.index());
+        return object != nullptr && object->id == id;
+    }
 
     ~SlotMap()
     {
         if constexpr (!std::is_trivial_v<StoredType>) {
             // call destructor on remaining objects if they are still alive
-            for (const auto &chunk : objectChunkTable_) {
+            for (const auto &chunk : chunkTable_) {
                 for (StoredInner_ &object : *chunk) {
-                    if (version(object.id) != FREE_VERSION) {
+                    if (object.id.alive()) {
                         reinterpret_cast<StoredType *>(object.storage)->~StoredType();
                     }
                 }
             }
         }
     }
+
+    struct sentinel {};
+    struct iterator {
+        SlotMap &sm;
+        uint32_t index{};
+
+        bool operator==(sentinel) const { return index == sm.size(); }
+        iterator &operator++()
+        {
+            ++index;
+            return *this;
+        }
+        StoredType &operator*()
+        {
+            auto *object = sm.objectAt(index);
+            CO_CORE_ASSERT(object != nullptr, "Object index was invalid!");
+            return *reinterpret_cast<StoredType *>(object->storage);
+        }
+    };
+
+    iterator begin() { return {*this, 0}; }
+    sentinel end() { return {}; }
 
   private:
     struct StoredInner_ {
@@ -141,28 +205,75 @@ template <typename StoredType> class SlotMap {
     };
     using Chunk = std::array<StoredInner_, CHUNK_SIZE>;
 
-    static uint32_t index(SlotMapHandle id) { return id.v & 0xFFFFFFFF; }
-    static uint32_t version(SlotMapHandle id) { return id.v >> 32; }
-    static SlotMapHandle makeHandle(uint32_t index, uint32_t version)
-    {
-        return {uint64_t{version} << 32 | index};
-    }
-
-    StoredInner_ &objectAt(uint32_t index)
+    StoredInner_ *objectAt(uint32_t index) const
     {
         const uint32_t chunkIndex = index / CHUNK_SIZE;
         const uint32_t elementIndex = index % CHUNK_SIZE;
 
-        if (chunkIndex >= objectChunkTable_.size()) {
-            throw std::range_error("Handle is out of range!");
+        if (chunkIndex >= chunkTable_.size()) { return nullptr; }
+        return &(*chunkTable_[chunkIndex])[elementIndex];
+    }
+
+    StoredInner_ &validatedGet(SlotMapHandle handle)
+    {
+        if (!handle.alive()) {
+            throw std::runtime_error{"Given handle is dead."}; //
         }
-        return (*objectChunkTable_[chunkIndex])[elementIndex];
+
+        if (handle.index() >= capacity()) {
+            throw std::runtime_error{fmt::format(
+                "Handle index {} is out of range ({}). Is the handle really from this slot map?!",
+                handle.index(),
+                size())};
+        }
+        const uint32_t chunkIndex = handle.index() / CHUNK_SIZE;
+        const uint32_t elementIndex = handle.index() % CHUNK_SIZE;
+
+        StoredInner_ &object = (*chunkTable_[chunkIndex])[elementIndex];
+        if (object.id != handle) {
+            CO_CORE_ASSERT(object.id.index() == handle.index(),
+                           "Object index mismatch ({} != {}) - object is in the wrong place!",
+                           object.id.index(),
+                           handle.index());
+            throw std::runtime_error{
+                fmt::format("Handle is outdated. Object version = {} but handle version = {}",
+                            object.id.version(),
+                            handle.version())};
+        }
+        return object;
     }
 
   private:
-    std::vector<std::unique_ptr<Chunk>> objectChunkTable_;
+    std::vector<std::unique_ptr<Chunk>> chunkTable_;
     std::vector<uint32_t> freeList_;
 };
+
+// convenience handle - less memory efficient but more convenient - stores the slotmap in order
+// to resolve the handle when needed
+template <typename StoredType> class ResolvableHandle {
+  public:
+    ResolvableHandle(SlotMap<StoredType> &slotMap, SlotMapHandle handle)
+        : slotMap_{slotMap}
+        , handle_{handle}
+    {
+    }
+
+    StoredType &operator*() { return slotMap_[handle_]; }
+    const StoredType &operator*() const { return slotMap_[handle_]; }
+
+    StoredType *operator->() { return &slotMap_[handle_]; }
+    const StoredType *operator->() const { return &slotMap_[handle_]; }
+
+    SlotMap<StoredType> &slotMap() { return slotMap_; }
+    const SlotMap<StoredType> &slotMap() const { return slotMap_; }
+    /* implicit */ operator SlotMapHandle() { return handle_; }
+    SlotMapHandle handle() const { return handle_; }
+
+  private:
+    SlotMap<StoredType> &slotMap_;
+    SlotMapHandle handle_;
+};
+
 } // namespace Cory
 
 /// make SlotMapHandle formattable
@@ -170,7 +281,7 @@ template <> struct fmt::formatter<Cory::SlotMapHandle> {
     template <typename ParseContext> constexpr auto parse(ParseContext &ctx) { return ctx.end(); }
     auto format(Cory::SlotMapHandle h, format_context &ctx)
     {
-        return fmt::format_to(ctx.out(), "{}${}", h.v & 0xFFFFFFFF, h.v >> 32);
+        return fmt::format_to(ctx.out(), "{}${}", h.index(), h.version());
     }
 };
 
--- Cory/test/SlotMap_Test.cpp
+++ Cory/test/SlotMap_Test.cpp
@@ -5,6 +5,37 @@
 #include <range/v3/all.hpp>
 #include <range/v3/view.hpp>
 
+TEST_CASE("SlotMapHandle", "[Cory/Base]")
+{
+    using namespace Cory;
+    SlotMapHandle h_invalid{};
+
+    CHECK(h_invalid.index() == SlotMapHandle::INVALID_INDEX);
+    CHECK(h_invalid.version() == SlotMapHandle::FREE_VERSION);
+    CHECK_FALSE(h_invalid.alive());
+
+    SlotMapHandle h1{0, 0};
+    CHECK(h1.index() == 0);
+    CHECK(h1.version() == 0);
+    CHECK(h1.alive());
+    CHECK_FALSE(h1.v & SlotMapHandle::FREE_BIT);
+
+    auto h1_v2 = SlotMapHandle::nextVersion(h1);
+    CHECK(h1_v2.index() == 0);
+    CHECK(h1_v2.version() == 1);
+    CHECK(h1_v2.alive());
+    CHECK_FALSE(h1_v2.v & SlotMapHandle::FREE_BIT);
+
+    auto h1_free = SlotMapHandle::setFreeBit(h1_v2);
+    CHECK(h1_free.index() == 0);
+    CHECK(h1_free.version() == (SlotMapHandle::FREE_VERSION | 1));
+    CHECK_FALSE(h1_free.alive());
+    CHECK(h1_free.v & SlotMapHandle::FREE_BIT);
+
+    auto h1_unfree = SlotMapHandle::clearFreeBit(h1_free);
+    CHECK(h1_unfree == h1_v2);
+}
+
 TEST_CASE("SlotMap<float>", "[Cory/Base]")
 {
     Cory::SlotMap<float> sm;
@@ -52,7 +83,7 @@ TEST_CASE("SlotMap<float>", "[Cory/Base]")
         auto h3 = sm.insert(3.0);
         CHECK(h3 != h2);
         auto h3_address = &sm[h3];
-        // ensure slots are reused but access through the ID still throws
+        // verify that slots are reused but access through the old handle still throws
         CHECK(h3_address == h2_address);
         CHECK_THROWS(sm[h2]);
     }
@@ -71,20 +102,24 @@ TEST_CASE("SlotMap<float>", "[Cory/Base]")
     }
 }
 
+static constexpr int UNINIT = 0;
+static constexpr int ALIVE = 1;
+static constexpr int DEAD = 2;
+
 // test struct that uses an int storage to track its lifetime
 struct Foo {
     Foo(int &bookkeep)
         : bookkeep_{bookkeep}
     {
-        bookkeep_ = 1;
+        bookkeep_ = ALIVE;
     }
     ~Foo()
     {
-        if (bookkeep_ != 1) {
+        if (bookkeep_ != ALIVE) {
             FAIL("already destructed or not properly constructed: bookkeep_ = " +
                  std::to_string(bookkeep_));
         }
-        bookkeep_ = 2;
+        bookkeep_ = DEAD;
     }
     int &bookkeep_;
 };
@@ -95,39 +130,39 @@ TEST_CASE("SlotMap<Foo>", "[Cory/Base]")
     SECTION("Correct construction and destruction of an object")
     {
         SlotMap<Foo> sm;
-        int book{0};
+        int book{UNINIT};
         auto h = sm.emplace(std::ref(book));
-        CHECK(book == 1);
+        CHECK(book == ALIVE);
         sm.release(h);
-        CHECK(book == 2);
+        CHECK(book == DEAD);
     }
 
     SECTION("Destruction of stored objects on Slotmap Destruction")
     {
-        int book1{0};
-        int book2{0};
+        int book1{UNINIT};
+        int book2{UNINIT};
         {
             SlotMap<Foo> sm;
             auto h1 = sm.emplace(std::ref(book1));
             auto h2 = sm.emplace(std::ref(book2));
-            CHECK(book1 == 1);
-            CHECK(book2 == 1);
+            CHECK(book1 == ALIVE);
+            CHECK(book2 == ALIVE);
 
             sm.release(h1);
-            CHECK(book1 == 2);
-            CHECK(book2 == 1);
+            CHECK(book1 == DEAD);
+            CHECK(book2 == ALIVE);
         }
 
-        CHECK(book1 == 2);
-        CHECK(book2 == 2);
+        CHECK(book1 == DEAD);
+        CHECK(book2 == DEAD);
     }
 
     SECTION("Correct construction and destruction of many stored objects")
     {
         std::srand(std::time(0));
         // poor man's fuzz-testing
-        std::vector<int> bookkeeper1(rand() % 512 + 1, 0);
-        std::vector<int> bookkeeper2(rand() % 512 + 1, 0);
+        std::vector<int> bookkeeper1(rand() % 512 + 1, UNINIT);
+        std::vector<int> bookkeeper2(rand() % 512 + 1, UNINIT);
         {
             SlotMap<Foo> sm;
 
@@ -136,16 +171,16 @@ TEST_CASE("SlotMap<Foo>", "[Cory/Base]")
                 ranges::views::transform([&](auto &b) { return sm.emplace(std::ref(b)); }) |
                 ranges::to<std::vector<SlotMapHandle>>();
 
-            CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == 1; }));
-            CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == 0; }));
+            CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == ALIVE; }));
+            CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == UNINIT; }));
 
             auto handles2 =
                 bookkeeper2 |
                 ranges::views::transform([&](auto &b) { return sm.emplace(std::ref(b)); }) |
                 ranges::to<std::vector<SlotMapHandle>>();
 
-            CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == 1; }));
-            CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == 1; }));
+            CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == ALIVE; }));
+            CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == ALIVE; }));
 
             // invalidating a bunch of handles
             auto handles3 = handles2 |
@@ -156,8 +191,8 @@ TEST_CASE("SlotMap<Foo>", "[Cory/Base]")
                 CHECK_THROWS(sm[h]);
             }
             // all objects are still alive
-            CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == 1; }));
-            CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == 1; }));
+            CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == ALIVE; }));
+            CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == ALIVE; }));
 
             // releasing the handles
             ranges::for_each(handles1, [&](auto &h) { sm.release(h); });
@@ -167,12 +202,47 @@ TEST_CASE("SlotMap<Foo>", "[Cory/Base]")
                 CHECK_THROWS(sm[h]);
             }
             // only values from set 2 are still alive
-            CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == 2; }));
-            CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == 1; }));
+            CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == DEAD; }));
+            CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == ALIVE; }));
         }
-        
+
         // after sm goes out of scope, all objects have been properly destructed
-        CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == 2; }));
-        CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == 2; }));
+        CHECK(ranges::all_of(bookkeeper1, [](const int &v) { return v == DEAD; }));
+        CHECK(ranges::all_of(bookkeeper2, [](const int &v) { return v == DEAD; }));
+    }
+
+    SECTION("Iterators and use with ranges")
+    {
+        std::srand(std::time(0));
+        std::vector<int> bookkeeper(rand() % 512 + 1, UNINIT);
+
+        SlotMap<Foo> sm;
+
+        auto handles = bookkeeper |
+                       ranges::views::transform([&](auto &b) { return sm.emplace(std::ref(b)); }) |
+                       ranges::to<std::vector<SlotMapHandle>>();
+
+        for (const Foo &f : sm) {
+            CHECK(f.bookkeep_ == ALIVE);
+        }
+
+        // CHECK(ranges::all_of(sm, [](const Foo& f) { return f.bookkeep_ == ALIVE; }));
+
+        ranges::for_each(handles | ranges::views::stride(3),
+                         [&](auto handle) { sm.release(handle); });
+        CHECK(
+            ranges::all_of(bookkeeper | ranges::views::stride(3), [](int b) { return b == DEAD; }));
+    }
+
+    SECTION("ResolvableHandle<Foo>")
+    {
+        SlotMap<Foo> sm;
+        int book{UNINIT};
+        auto h = sm.emplace(std::ref(book));
+        ResolvableHandle rh{sm, h};
+        CHECK(rh->bookkeep_ == ALIVE);
+
+        const ResolvableHandle crh{rh};
+        CHECK(crh->bookkeep_ == ALIVE);
     }
 }

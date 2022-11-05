066344d9c6a89351564e9ba891b843298fca01e9
Jakob Weiss <jakob.jw.weiss@gmail.com>
2022-11-05 23:35:06 +0100

    refactor mesh creation into static functions
    also, some other random shit

---
+++ Cory/include/Cory/Application/DynamicGeometry.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+#include <Cory/Renderer/Common.hpp>
+
+namespace Cory {
+class DynamicGeometry {
+  public:
+
+#pragma pack(push, 1)
+    struct Vertex {
+        glm::vec3 pos;
+        glm::vec3 tex;
+        glm::vec4 col;
+    };
+#pragma pack(pop)
+    // just verifying that the layout is ok - one vertex is supposed to be 10 floats
+    static_assert(sizeof(Vertex) == 10 * sizeof(float));
+
+    /// create an equilateral triangle mesh in XY
+    static Magnum::Vk::Mesh createTriangle(Context &ctx, uint32_t binding = 0);
+    /// create a unit cube centered around the @a offset
+    static Magnum::Vk::Mesh createCube(Context &ctx, glm::vec3 offset = {}, uint32_t binding = 0);
+};
+} // namespace Cory

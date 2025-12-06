48d92cc796f6fa53a4ad5925911e9203f35bd743
Jakob Weiss <jakob.jw.weiss@gmail.com>
2025-12-06 22:23:24 +0100

    Update Dynamic geometry generation

--- src/Cory/Application/DynamicGeometry.cpp
+++ src/Cory/Application/DynamicGeometry.cpp
@@ -1,6 +1,7 @@
 #include <Cory/Application/DynamicGeometry.hpp>
 
 #include <Cory/Renderer/Context.hpp>
+#include <KDGpu/buffer_options.h>
 
 #include <glm/trigonometric.hpp> // for glm::radians
 #include <glm/vec2.hpp>
@@ -43,87 +44,124 @@ Mesh DynamicGeometry::createTriangle(Context &ctx, uint32_t binding)
 
 Mesh DynamicGeometry::createCube(Context &ctx, glm::vec3 offset, uint32_t binding)
 {
-    // struct VertexTemp {
-    //     glm::vec3 pos;
-    //     glm::vec3 norm;
-    //     glm::vec3 col;
-    // };
-    // std::vector<VertexTemp> vertices{
-    //     // left face (white)
-    //     {{-.5f, -.5f, -.5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f}},
-    //     {{-.5f, -.5f, .5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f}},
-    //     {{-.5f, .5f, .5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f}},
-    //     {{-.5f, -.5f, -.5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f}},
-    //     {{-.5f, .5f, .5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f}},
-    //     {{-.5f, .5f, -.5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f}},
-    //
-    //     // right face (yellow)
-    //     {{.5f, -.5f, -.5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f}},
-    //     {{.5f, .5f, .5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f}},
-    //     {{.5f, -.5f, .5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f}},
-    //     {{.5f, -.5f, -.5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f}},
-    //     {{.5f, .5f, -.5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f}},
-    //     {{.5f, .5f, .5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f}},
-    //
-    //     // top face (orange, remember y axis points down)
-    //     {{-.5f, -.5f, -.5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f}},
-    //     {{.5f, -.5f, .5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f}},
-    //     {{-.5f, -.5f, .5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f}},
-    //     {{-.5f, -.5f, -.5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f}},
-    //     {{.5f, -.5f, -.5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f}},
-    //     {{.5f, -.5f, .5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f}},
-    //
-    //     // bottom face (red)
-    //     {{-.5f, .5f, -.5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f}},
-    //     {{-.5f, .5f, .5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f}},
-    //     {{.5f, .5f, .5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f}},
-    //     {{-.5f, .5f, -.5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f}},
-    //     {{.5f, .5f, .5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f}},
-    //     {{.5f, .5f, -.5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f}},
-    //
-    //     // nose face (blue)
-    //     {{-.5f, -.5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f}},
-    //     {{.5f, .5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f}},
-    //     {{-.5f, .5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f}},
-    //     {{-.5f, -.5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f}},
-    //     {{.5f, -.5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f}},
-    //     {{.5f, .5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f}},
-    //
-    //     // tail face (green)
-    //     {{-.5f, -.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f}},
-    //     {{-.5f, .5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f}},
-    //     {{.5f, .5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f}},
-    //     {{-.5f, -.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f}},
-    //     {{.5f, .5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f}},
-    //     {{.5f, -.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f}},
-    //
-    // };
-    // for (auto &v : vertices) {
-    //     v.pos += offset;
-    // }
-    //
-    // Vk::Mesh mesh(ctx.defaultMeshLayout());
-    //
-    // Vk::Buffer vBuffer{
-    //     ctx.device(),
-    //     Vk::BufferCreateInfo{Vk::BufferUsage::VertexBuffer, vertices.size() * sizeof(Vertex)},
-    //     Magnum::Vk::MemoryFlag::HostCoherent | Magnum::Vk::MemoryFlag::HostVisible};
-    //
-    // Corrade::Containers::Array<char, Vk::MemoryMapDeleter> data =
-    // vBuffer.dedicatedMemory().map();
-    //
-    // auto view = reinterpret_cast<Vertex *>(data.data());
-    // for (gsl::index i = 0; i < vertices.size(); ++i) {
-    //     view[i] = Vertex{.pos = vertices[i].pos + offset,
-    //                      .normal = vertices[i].norm,
-    //                      .col = glm::vec4{vertices[i].col, 1.0f}};
-    // }
-    //
-    // mesh.addVertexBuffer(0, std::move(vBuffer),
-    // 0).setCount(gsl::narrow<uint32_t>(vertices.size()));
-    //
-    // return mesh;
-    return {};
+    std::vector<Mesh::Vertex> vertices{
+        // left face (white)
+        {{-.5f, -.5f, -.5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f, 1.0f}},
+        {{-.5f, -.5f, .5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f, 1.0f}},
+        {{-.5f, .5f, .5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f, 1.0f}},
+        {{-.5f, -.5f, -.5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f, 1.0f}},
+        {{-.5f, .5f, .5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f, 1.0f}},
+        {{-.5f, .5f, -.5f}, {-1.0f, 0.0f, 0.0f}, {.9f, .9f, .9f, 1.0f}},
+
+        // right face (yellow)
+        {{.5f, -.5f, -.5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f, 1.0f}},
+        {{.5f, .5f, .5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f, 1.0f}},
+        {{.5f, -.5f, .5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f, 1.0f}},
+        {{.5f, -.5f, -.5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f, 1.0f}},
+        {{.5f, .5f, -.5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f, 1.0f}},
+        {{.5f, .5f, .5f}, {1.0f, 0.0f, 0.0f}, {.8f, .8f, .1f, 1.0f}},
+
+        // top face (orange, remember y axis points down)
+        {{-.5f, -.5f, -.5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f, 1.0f}},
+        {{.5f, -.5f, .5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f, 1.0f}},
+        {{-.5f, -.5f, .5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f, 1.0f}},
+        {{-.5f, -.5f, -.5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f, 1.0f}},
+        {{.5f, -.5f, -.5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f, 1.0f}},
+        {{.5f, -.5f, .5f}, {0.0f, -1.0f, 0.0f}, {.9f, .6f, .1f, 1.0f}},
+
+        // bottom face (red)
+        {{-.5f, .5f, -.5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f, 1.0f}},
+        {{-.5f, .5f, .5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f, 1.0f}},
+        {{.5f, .5f, .5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f, 1.0f}},
+        {{-.5f, .5f, -.5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f, 1.0f}},
+        {{.5f, .5f, .5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f, 1.0f}},
+        {{.5f, .5f, -.5f}, {0.0f, 1.0f, 0.0f}, {.8f, .1f, .1f, 1.0f}},
+
+        // nose face (blue)
+        {{-.5f, -.5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f, 1.0f}},
+        {{.5f, .5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f, 1.0f}},
+        {{-.5f, .5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f, 1.0f}},
+        {{-.5f, -.5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f, 1.0f}},
+        {{.5f, -.5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f, 1.0f}},
+        {{.5f, .5f, 0.5f}, {0.0f, 0.0f, 1.0f}, {.1f, .1f, .8f, 1.0f}},
+
+        // tail face (green)
+        {{-.5f, -.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f, 1.0f}},
+        {{-.5f, .5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f, 1.0f}},
+        {{.5f, .5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f, 1.0f}},
+        {{-.5f, -.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f, 1.0f}},
+        {{.5f, .5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f, 1.0f}},
+        {{.5f, -.5f, -0.5f}, {0.0f, 0.0f, -1.0f}, {.1f, .8f, .1f, 1.0f}},
+
+    };
+    // 36 vertices, 12 triangles (2 per face), each triangle needs 3 indices
+    std::vector<uint32_t> indices;
+    for (uint32_t i = 0; i < vertices.size(); i += 3) {
+        indices.push_back(i);
+        indices.push_back(i + 1);
+        indices.push_back(i + 2);
+    }
+    // apply offset
+    for (auto &v : vertices) {
+        v.pos += offset;
+    }
+
+    return createFromCpuBuffers(ctx, vertices, indices);
+}
+
+Mesh DynamicGeometry::createFromCpuBuffers(Context &ctx,
+                                           std::span<const Mesh::Vertex> vertexData,
+                                           std::span<const uint32_t> indexData)
+{
+    auto &device = ctx.device();
+    KDGpu::UploadStagingBuffer vertex_staging_buffer;
+    KDGpu::UploadStagingBuffer index_staging_buffer;
+    Mesh mesh;
+
+    {
+        const KDGpu::DeviceSize dataByteSize = vertexData.size() * sizeof(Mesh::Vertex);
+        const KDGpu::BufferOptions bufferOptions = {
+            .label = "Vertex Buffer",
+            .size = dataByteSize,
+            .usage = KDGpu::BufferUsageFlagBits::VertexBufferBit |
+                     KDGpu::BufferUsageFlagBits::TransferDstBit,
+            .memoryUsage = KDGpu::MemoryUsage::GpuOnly};
+
+        mesh.vertexBuffer = device.createBuffer(bufferOptions);
+
+        const KDGpu::BufferUploadOptions uploadOptions = {
+            .destinationBuffer = mesh.vertexBuffer,
+            .dstStages = KDGpu::PipelineStageFlagBit::VertexAttributeInputBit,
+            .dstMask = KDGpu::AccessFlagBit::VertexAttributeReadBit,
+            .data = vertexData.data(),
+            .byteSize = dataByteSize};
+
+        vertex_staging_buffer = ctx.graphicsQueue().uploadBufferData(uploadOptions);
+    }
+    // Create a buffer to hold the geometry index data
+    {
+        const KDGpu::DeviceSize dataByteSize = indexData.size() * sizeof(uint32_t);
+        const KDGpu::BufferOptions bufferOptions = {.label = "Index Buffer",
+                                                    .size = dataByteSize,
+                                                    .usage =
+                                                        KDGpu::BufferUsageFlagBits::IndexBufferBit |
+                                                        KDGpu::BufferUsageFlagBits::TransferDstBit,
+                                                    .memoryUsage = KDGpu::MemoryUsage::GpuOnly};
+        mesh.indexBuffer = device.createBuffer(bufferOptions);
+        const KDGpu::BufferUploadOptions uploadOptions = {
+            .destinationBuffer = mesh.indexBuffer,
+            .dstStages = KDGpu::PipelineStageFlagBit::IndexInputBit,
+            .dstMask = KDGpu::AccessFlagBit::IndexReadBit,
+            .data = indexData.data(),
+            .byteSize = dataByteSize};
+        index_staging_buffer = ctx.graphicsQueue().uploadBufferData(uploadOptions);
+    }
+
+    // Ensure upload is finished.
+    vertex_staging_buffer.fence.wait();
+    index_staging_buffer.fence.wait();
+
+    return mesh;
 }
 
 } // namespace Cory
--- src/Cory/Application/DynamicGeometry.hpp
+++ src/Cory/Application/DynamicGeometry.hpp
@@ -5,18 +5,12 @@
 
 #include <Cory/Renderer/Common.hpp>
 #include <KDGpu/buffer.h>
+#include <KDGpu/graphics_pipeline_options.h>
 
 namespace Cory {
 
+// simple mesh structure with vertex and index buffers
 struct Mesh {
-    Gpu::Buffer vertexBuffer;
-    Gpu::Buffer indexBuffer;
-    size_t vertexCount;
-    size_t indexCount;
-};
-
-class DynamicGeometry {
-  public:
 #pragma pack(push, 1)
     struct Vertex {
         glm::vec3 pos;
@@ -27,9 +21,46 @@ class DynamicGeometry {
     // just verifying that the layout is ok - one vertex is supposed to be 10 floats
     static_assert(sizeof(Vertex) == 10 * sizeof(float));
 
+    static constexpr std::vector<Gpu::VertexAttribute> vertexAttributes()
+    {
+        return {{
+            {
+                // position
+                .location = 0,
+                .format = Gpu::Format::R32G32B32_SFLOAT,
+                .offset = offsetof(Mesh::Vertex, pos),
+            },
+            {
+                // normal
+                .location = 1,
+                .format = Gpu::Format::R32G32B32_SFLOAT,
+                .offset = offsetof(Mesh::Vertex, normal),
+            },
+            {
+                // color
+                .location = 2,
+                .format = Gpu::Format::R32G32B32A32_SFLOAT,
+                .offset = offsetof(Mesh::Vertex, col),
+            },
+        }};
+    }
+
+    Gpu::Buffer vertexBuffer;
+    Gpu::Buffer indexBuffer;
+    size_t vertexCount;
+    size_t indexCount;
+};
+
+class DynamicGeometry {
+  public:
     /// create an equilateral triangle mesh in XY
     static Mesh createTriangle(Context &ctx, uint32_t binding = 0);
     /// create a unit cube centered around the @a offset
     static Mesh createCube(Context &ctx, glm::vec3 offset = {}, uint32_t binding = 0);
+
+    // Generic function that handles vertex and index buffer creation & upload
+    static Mesh createFromCpuBuffers(Context &ctx,
+                                     std::span<const Mesh::Vertex> vertexData,
+                                     std::span<const uint32_t> indexData);
 };
 } // namespace Cory
